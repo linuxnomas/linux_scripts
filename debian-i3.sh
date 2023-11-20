4eb54823d4d13ce253bc877c045095056978e64e
LinuxNomad <zarathustra.f90@gmail.com>
2023-11-20 07:36:49 +0200

    echo changes in debian-i3.sh

--- debian-i3.sh
+++ debian-i3.sh
@@ -1,14 +1,14 @@
-echo ####################
-echo # Install packages #
-echo ####################
+echo ++++++++++++++++++++
+echo + Install packages +
+echo ++++++++++++++++++++
 sudo apt install i3 i3blocks lightdm x11-xserver-utils -y
 sudo apt install pulseaudio nm-tray fonts-font-awesome -y
 sudo apt install curl git bc feh -y
 sudo apt install vim xfe xfe-themes -y
 
-echo ###########################
-echo # Install Thorium browser #
-echo ###########################
+echo +++++++++++++++++++++++++++
+echo + Install Thorium browser +
+echo +++++++++++++++++++++++++++
 wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
 sudo mv thorium.list /etc/apt/sources.list.d/
 sudo apt update -y
